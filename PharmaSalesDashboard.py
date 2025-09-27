23fd9dfc9177f870ce09468b7e0eae029d591053
Mahmoudkamel95 <drmahmoudkamel174@gmail.com>
2025-09-27 11:55:45 +0300

    Update PharmaSalesDashboard.py

--- PharmaSalesDashboard.py
+++ PharmaSalesDashboard.py
@@ -22,8 +22,8 @@ if f1 is not None:
     st.write(filename)
     df=pd.read_csv(filename,encoding= "ISO-8859-1")
 else:
-url = "https://drive.google.com/uc?id=1kaDONiLBMdYIiufE0iAsZEosVBjsaLMG"
-df = pd.read_csv(url, encoding="ISO-8859-1")
+   url = "https://drive.google.com/uc?id=1kaDONiLBMdYIiufE0iAsZEosVBjsaLMG"
+   df = pd.read_csv(url, encoding="ISO-8859-1")
 
 # sidebar
 st.sidebar.header("Pharmaceutical Dashboard")
@@ -168,6 +168,7 @@ with st.expander("View Data"):
 # Download original DataSet  
 csv = df.to_csv(index = False).encode("utf=8")
 st.download_button("Download Data",data = csv , file_name = "Data.csv",mime = "text/csv")
+
 
 
 
